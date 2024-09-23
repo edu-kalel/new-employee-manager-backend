6a586e1c601ad444b91f416e73818257bcc1ed06
eliumendoza <emendozag1602@gmail.com>
2024-09-23 11:27:36 -0600

    adds endpoints to get charges and states

--- src/main/java/com/ipn/escom/lit/hrmanagerss/hrmanagerss/config/AuthenticationService.java
+++ src/main/java/com/ipn/escom/lit/hrmanagerss/hrmanagerss/config/AuthenticationService.java
@@ -1,8 +1,11 @@
 package com.ipn.escom.lit.hrmanagerss.hrmanagerss.config;
 
+import com.ipn.escom.lit.hrmanagerss.hrmanagerss.presentation.dto.EstadoDTO;
+import com.ipn.escom.lit.hrmanagerss.hrmanagerss.presentation.dto.PuestoDTO;
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.presentation.dto.RegistroEmpleadoDTO;
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.repository.EmpleadoRepo;
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.service.AdminService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -44,5 +47,13 @@ public class AuthenticationService {
       .nombre(empleado.getName())
       .email(empleado.getEmail())
       .build();
+  }
+
+  public List<PuestoDTO> getPuestosList() {
+    return adminService.getPuestosList();
+  }
+
+  public List<EstadoDTO> getEstadosList() {
+    return adminService.getEstadosList();
   }
 }
--- src/main/java/com/ipn/escom/lit/hrmanagerss/hrmanagerss/controller/AuthenticationController.java
+++ src/main/java/com/ipn/escom/lit/hrmanagerss/hrmanagerss/controller/AuthenticationController.java
@@ -3,11 +3,16 @@ package com.ipn.escom.lit.hrmanagerss.hrmanagerss.controller;
 
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.config.AuthenticationRequest;
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.config.AuthenticationResponse;
+import com.ipn.escom.lit.hrmanagerss.hrmanagerss.presentation.dto.EstadoDTO;
+import com.ipn.escom.lit.hrmanagerss.hrmanagerss.presentation.dto.PuestoDTO;
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.presentation.dto.RegistroEmpleadoDTO;
 import com.ipn.escom.lit.hrmanagerss.hrmanagerss.config.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +34,17 @@ public class AuthenticationController {
   @PostMapping("/authenticate")
   public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
     return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+  }
+
+  @GetMapping("/puestos-list")
+  @Operation(summary = "Obtener lista de puestos")
+  public ResponseEntity<List<PuestoDTO>> getAllPuestos(){
+    return new ResponseEntity<>(authenticationService.getPuestosList(), HttpStatus.OK);
+  }
+
+  @GetMapping("/states-list")
+  @Operation(summary = "Obtener lista de estados")
+  public ResponseEntity<List<EstadoDTO>> getAllStates(){
+    return new ResponseEntity<>(authenticationService.getEstadosList(), HttpStatus.OK);
   }
 }
